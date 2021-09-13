2d4058885e55bce97ecfff2201f9aaba169a7c5a
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-13 16:52:17 -0500

    Add register and login

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
+const User = require('../users/users-model')
+const bcrypt = require('bcryptjs')
 const {
   checkPasswordLength,
   checkUsernameExists,
@@ -6,11 +8,29 @@ const {
 } = require('./auth-middleware')
 
 router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => { 
-  res.json('register')
+  const { username, password } = req.body
+  const hash = bcrypt.hashSync(password, 8)
+
+  User.add({ username, password: hash })
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(next)
 })
 
 router.post('/login', checkUsernameExists, (req, res, next) => { 
-  res.json('login')
+  const { password } = req.body
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user
+    res.json({
+      message: `Welcome ${req.user.username}!`
+    })
+  } else {
+    next({
+      status: 401,
+      message: 'Invalid credentials'
+    })
+  }
 })
 
 router.get('/logout', (req, res, next) => { 
