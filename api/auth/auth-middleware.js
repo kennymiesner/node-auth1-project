c991dba297e37ab4a2c7b9e177ffe0cc4e7baf63
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-13 16:24:11 -0500

    Add middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,63 +1,55 @@
+const User = require('../users/users-model')
+
+function restricted(req, res, next) {
+  next()
+}
+
+async function checkUsernameFree(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username })
+    if (!users.length) {
+      next()
+    } else {
+      next({
+        status: 422,
+        message: 'Username taken'
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username })
+    if (users.length) {
+      next()
+    } else {
+      next({
+        status: 401,
+        message: 'Invalid credentials'
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
+function checkPasswordLength(req, res, next) {
+  if (!req.body.password || req.body.password.length < 3) {
+    next({
+      status: 422,
+      message: 'Password must be longer than 3 chars'
+    })
+  } else {
+    next()
+  }
+}
+
 module.exports = {
   restricted,
   checkUsernameFree,
   checkUsernameExists,
   checkPasswordLength,
 }
-
-/*
-  If the user does not have a session saved in the server
-
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
-*/
-const restricted = (req, res, next) => {
-  if (req.session.user) {
-    next()
-  } else (
-    next({
-      status: 401,
-      message: 'You shall not pass!'
-    })
-  )
-}
-
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
-function checkUsernameFree() {
-
-}
-
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
-function checkUsernameExists() {
-
-}
-
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
-function checkPasswordLength() {
-
-}
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,71 +1,20 @@
-// Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
-// middleware functions from `auth-middleware.js`. You will need them here!
 const router = require('express').Router()
+const {
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree,
+} = require('./auth-middleware')
 
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
-  }
-
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
-
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
- */
-router.post('/register', (req, res, next) => { // eslint-disable-line
+router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => { // eslint-disable-line
   res.json('register')
 })
 
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
- */
-router.post('/login', (req, res, next) => { // eslint-disable-line
+router.post('/login', checkUsernameExists, (req, res, next) => { // eslint-disable-line
   res.json('login')
 })
 
-/**
-  3 [GET] /api/auth/logout
-
-  response for logged-in users:
-  status 200
-  {
-    "message": "logged out"
-  }
-
-  response for not-logged-in users:
-  status 200
-  {
-    "message": "no session"
-  }
- */
 router.get('/logout', (req, res, next) => { // eslint-disable-line
   res.json('logout')
 })
 
- 
-// Don't forget to add the router to the `exports` object so it can be required in other modules
 module.exports = router
